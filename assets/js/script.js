e1abe33069e754d97578e0c3286468ca43268788
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2020-12-30 12:58:31 -0600

    added password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-// Assignment code here
+/* GENERATOR FUNCTIONS */
+
+// function for password prompts
+function promptMe(){
+  var length = Number(prompt("How many characters would you like in your password?"));
+  while (isNaN(length) || length < 8 || length > 128) length=Number(prompt("Length must be between 8 - 128 characters. How many characters would you like in your password?"));
+
+}
+
 
 
 // Get references to the #generate element
