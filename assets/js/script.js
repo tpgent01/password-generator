ae872aed497cf740b601452cb011e9fe419ee269
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2020-12-30 13:44:46 -0600

    added password prompts and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,24 @@ function promptMe(){
   while (isNaN(length) || length < 8 || length > 128) length=Number(prompt("Length must be between 8 - 128 characters. How many characters would you like in your password?"));
   console.log (length);
 
+
   //password criteria prompts
   var upper = confirm("Would you like uppercase letters?");
   var lower = confirm("Would you like lowercase letters?");
   var numbers = confirm("Would you like to use numbers?");
   var special = confirm("Would you like to use special characters?");
   console.log (upper, lower, numbers, special);
-}
 
-
+  //checks if user criteria is valid
+  while (!upper && !lower && !numbers && !special) {
+    alert("You must selected at least one character type.");
+      upper = confirm("Would you like uppercase letters?");
+      lower = confirm("Would you like lowercase letters?");
+      numbers = confirm("Would you like to use numbers?");
+      special = confirm("Would you like to use special characters?");
+      console.log (upper, lower, numbers, special);
+    }
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
