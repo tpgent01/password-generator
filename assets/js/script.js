466eab59967437167ed58d3dc04ad12011aad087
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2020-12-30 13:10:18 -0600

    added criteria prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,17 @@
 
 // function for password prompts
 function promptMe(){
+  // password length prompt
   var length = Number(prompt("How many characters would you like in your password?"));
   while (isNaN(length) || length < 8 || length > 128) length=Number(prompt("Length must be between 8 - 128 characters. How many characters would you like in your password?"));
+  console.log (length);
 
+  //password criteria prompts
+  var upper = confirm("Would you like uppercase letters?");
+  var lower = confirm("Would you like lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var special = confirm("Would you like to use special characters?");
+  console.log (upper, lower, numbers, special);
 }
 
 
